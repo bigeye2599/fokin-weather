92cffff4985dc8aee7e9dd4847eb3f662acf3481
thlee <thlee@t5online.com>
2021-04-23 12:52:45 +0900

    #1.3 Asking for Permissions 하는중 (4:12)

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLocation = async () => {
+    try {
+      const response = await Location.requestForegroundPermissionsAsync();
+      console.log("response : ", response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log("location : ", location);
+    } catch (error) {
+      Alert("Cna't find you.", "So sad", error);
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
